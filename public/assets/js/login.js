4dfa0b0fbc2238d111d873ef0d548aedc9bdd14b
jypkr <wodusqkr1210@gmail.com>
2021-09-21 10:42:32 -0700

    0921-1042

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,21 +1,19 @@
-const { bootstrap } = window
+const { bulma } = window
 
 
 document.getElementById('login').addEventListener('click', event => {
-	console.log('login clicked')
-	console.log(document.getElementById('lUsername').value)
-	// event.preventDefault()
-	// axios.post('/api/users/login', {
-	// 	username: document.getElementById('lUsername').value,
-	// 	password: document.getElementById('lPassword').value
-	// })
-	// 	.then(({ data: token }) => {
-	// 		if (token) {
-	// 			localStorage.setItem('token', token)
-	// 			window.location = '/'
-	// 		} else {
-	// 			alert('Invalid username or password.')
-	// 		}
-	// 	})
-	// 	.catch(err => console.error(err))
+	event.preventDefault()
+	axios.post('/api/users/login', {
+		username: document.getElementById('lUsername').value,
+		password: document.getElementById('lPassword').value
+	})
+		.then(({ data: token }) => {
+			if (token) {
+				localStorage.setItem('token', token)
+				window.location = '/'
+			} else {
+				alert('Invalid username or password.')
+			}
+		})
+		.catch(err => console.error(err))
 })
