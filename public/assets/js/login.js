0d53038c19aa6e9c80b7cf154d1f13979984307a
Lan Bui <lantbui27@gmail.com>
2021-09-24 14:04:54 -0700

    change order of login.js code

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,15 +1,5 @@
 const { bootstrap } = window
 
-document.getElementById('register').addEventListener('click', event => {
-	event.preventDefault()
-	axios.post('/api/users/register', {
-		username: document.getElementById('username').value,
-		password: document.getElementById('password').value
-	})
-		.then(() => alert('User registered! Log in.'))
-		.catch(err => console.error(err))
-})
-
 document.getElementById('login').addEventListener('click', event => {
 	event.preventDefault()
 	axios.post('/api/users/login', {
@@ -26,3 +16,14 @@ document.getElementById('login').addEventListener('click', event => {
 		})
 		.catch(err => console.error(err))
 })
+
+document.getElementById('register').addEventListener('click', event => {
+	event.preventDefault()
+	axios.post('/api/users/register', {
+		username: document.getElementById('username').value,
+		password: document.getElementById('password').value
+	})
+		.then(() => alert('User registered! Log in.'))
+		.catch(err => console.error(err))
+})
+
