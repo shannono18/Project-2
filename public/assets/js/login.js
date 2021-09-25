3a47404f307c33abb716c14b7137c628d4a7c8d6
Lan Bui <lantbui27@gmail.com>
2021-09-25 12:44:44 -0700

    register.js created

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -17,13 +17,13 @@ document.getElementById('login').addEventListener('click', event => {
 		.catch(err => console.error(err))
 })
 
-document.getElementById('register').addEventListener('click', event => {
-	event.preventDefault()
-	axios.post('/api/users/register', {
-		username: document.getElementById('username').value,
-		password: document.getElementById('password').value
-	})
-		.then(() => alert('User registered! Log in.'))
-		.catch(err => console.error(err))
-})
+// document.getElementById('register').addEventListener('click', event => {
+// 	event.preventDefault()
+// 	axios.post('/api/users/register', {
+// 		username: document.getElementById('username').value,
+// 		password: document.getElementById('password').value
+// 	})
+// 		.then(() => alert('User registered! Log in.'))
+// 		.catch(err => console.error(err))
+// })
 
